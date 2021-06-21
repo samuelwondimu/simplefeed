c2e980f3d814cbd0d3a0d2f11b5f12928d050e61
samuelwondimu <wsamuelwondimu@gmail.com>
2021-06-21 11:32:53 +0300

    fix mongo error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
-import * as mongoose from 'mongoose';
-import Controller from './interfaces/controller.interface';
-import errorMiddleware from './middleware/error.middleware';
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as mongoose from "mongoose";
+import Controller from "./interfaces/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
 
 class App {
   public app: express.Application;
@@ -38,18 +38,25 @@ class App {
 
   private initializeControllers(controllers: Controller[]) {
     controllers.forEach((controller) => {
-      this.app.use('/', controller.router);
+      this.app.use("/", controller.router);
     });
   }
 
-  private connectToTheDatabase() {
+  private connectToTheDatabase(): void {
     const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
-    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    mongoose
+      .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+      .then((result) => {
+        console.log("Mongo Connected", result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
